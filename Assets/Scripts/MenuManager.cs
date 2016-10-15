ec4c92bae93b145827bcae5cfb398cadafc34f29
Erwan AZZOUG <erwan.azzoug@etu.univ-nantes.fr>
2016-10-15 15:41:55 +0200

    Ajout du menu + sprites + font

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Sprite pressedSprite = Resources.Load<Sprite>("Sprites/button-dark.png");
+        //Sprite defaultSprite = Resources.Load<Sprite>("Sprites/button-dark-outline.png");
+        SpriteState st = new SpriteState();
+        st.pressedSprite = pressedSprite;
+        GetComponent<Button>().spriteState = st;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
