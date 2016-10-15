e43a35fcade96bc366f2373ba511c2d1abd8b28f
Argann BONNEAU <argann.bonneau@etu.univ-nantes.fr>
2016-10-15 11:50:42 +0200

    Ajout du système de tir, avec prise en charge de la vie des ennemis

---
+++ Assets/Scripts/Ammo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Ammo : MonoBehaviour {
+
+    [SerializeField]
+    private int hitPoints;
+
+    public int HitPoints {
+        get { return hitPoints; }
+        set { hitPoints = value; }
+    }
+
+    void Start() {
+        gameObject.tag = "Ammo";
+    }
+
+
+
+}
