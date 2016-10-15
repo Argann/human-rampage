6d92c4f9d63c809884daaa4795dcbdd994b46823
Argann BONNEAU <argann.bonneau@etu.univ-nantes.fr>
2016-10-15 15:35:55 +0200

    Ajout du système de scoring

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class ScoreManager : MonoBehaviour {
 
+    private int score;
+
 	// Use this for initialization
 	void Start () {
-	
+        score = 0;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	public void AddScore(int sc) {
+        score += sc;
+    }
+
+    public int GetScore() {
+        return score;
+    }
+
+    public static ScoreManager GetManager() {
+        return GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+    }
 }
