ce98de65737f1365acceb16a753025e735f1432f
Argann BONNEAU <argann.bonneau@etu.univ-nantes.fr>
2016-10-15 17:20:55 +0200

    Ajout de l'ui trop bien, du score de heal

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    [SerializeField]
+    private Text score;
+
+    [SerializeField]
+    private Text life;
+    [SerializeField]
+    private Image life_image;
+	
+	// Update is called once per frame
+	void Update () {
+        life.text = GameManager.GetManager().CurrentPlayerHealth + "/" + GameManager.GetManager().PlayerHealth;
+        life_image.fillAmount = (float)GameManager.GetManager().CurrentPlayerHealth / (float)GameManager.GetManager().PlayerHealth;
+        score.text = ""+ScoreManager.GetManager().GetScore();
+
+	}
+}
